55532c1b1aefe6901e6ad38af72ee11238bdc783
AlexNorman96 <alex.norman15@hotmail.co.uk>
2018-06-30 15:44:14 +0100

    added q1 answer

---
+++ Question_1.py
@@ -0,0 +1,19 @@
+# Count the number of As, Cs, Ts and Gs in a sequence. 
+
+DNA = "my_seq"
+
+A_count = DNA.count("A")
+
+C_count = DNA.count("C")
+
+T_count = DNA.count("T")
+
+G_count = DNA.count("G")
+
+print(A_count)
+
+print(C_count)
+
+print(T_count)
+
+print(G_count)
